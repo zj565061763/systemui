63510db5db661be89ea92e1dc163f27be4d6e819
zjun <565061763@qq.com>
2020-09-11 16:15:25 +0800

    FStatusBarPaddingLayout

---
+++ lib/src/main/java/com/sd/lib/systemui/statusbar/view/FStatusBarPaddingLayout.java
@@ -0,0 +1,61 @@
+package com.sd.lib.systemui.statusbar.view;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import com.sd.lib.systemui.statusbar.FStatusBarUtils;
+
+public class FStatusBarPaddingLayout extends FrameLayout
+{
+    private boolean mPaddingStatusBar = true;
+    private int mTopPadding;
+
+    public FStatusBarPaddingLayout(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        mTopPadding = getPaddingTop();
+    }
+
+    public void setPaddingStatusBar(boolean padding)
+    {
+        if (mPaddingStatusBar != padding)
+        {
+            mPaddingStatusBar = padding;
+            checkPaddingStatusBar();
+        }
+    }
+
+    private void checkPaddingStatusBar()
+    {
+        if (mPaddingStatusBar)
+        {
+            final int barHeight = FStatusBarUtils.getActivityStatusBarHeight(getContext());
+            super.setPadding(getPaddingLeft(), barHeight, getPaddingRight(), getPaddingBottom());
+        } else
+        {
+            super.setPadding(getPaddingLeft(), mTopPadding, getPaddingRight(), getPaddingBottom());
+        }
+    }
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom)
+    {
+        mTopPadding = top;
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        checkPaddingStatusBar();
+    }
+
+    @Override
+    protected void onAttachedToWindow()
+    {
+        super.onAttachedToWindow();
+        checkPaddingStatusBar();
+    }
+}
