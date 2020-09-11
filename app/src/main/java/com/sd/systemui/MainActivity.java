d27d8a2d14f7837bc1ad80e0f70b67ee586a1857
zjun <565061763@qq.com>
2020-09-11 11:03:14 +0800

    demo

--- app/src/main/java/com/sd/systemui/MainActivity.java
+++ app/src/main/java/com/sd/systemui/MainActivity.java
@@ -1,16 +1,32 @@
 package com.sd.systemui;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.sd.lib.systemui.statusbar.FStatusBarUtils;
+import com.sd.systemui.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private ActivityMainBinding mBinding;
+    private boolean mDarkStatusBar = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        FStatusBarUtils.setTransparent(getWindow(), mDarkStatusBar);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        mDarkStatusBar = !mDarkStatusBar;
+        FStatusBarUtils.setTransparent(getWindow(), mDarkStatusBar);
     }
 }
