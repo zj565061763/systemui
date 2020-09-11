ac6b87d1c45f42f4c4fffdf825d89d7f1cc53e67
zjun <565061763@qq.com>
2020-09-11 11:35:00 +0800

    FStatusBarUtils

--- lib/src/main/java/com/sd/lib/systemui/statusbar/FStatusBarUtils.java
+++ lib/src/main/java/com/sd/lib/systemui/statusbar/FStatusBarUtils.java
@@ -1,6 +1,7 @@
 package com.sd.lib.systemui.statusbar;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
@@ -44,40 +45,6 @@ public class FStatusBarUtils
     }
 
     /**
-     * 半透明状态栏
-     */
-    public static void setHalfTransparent(Window window)
-    {
-        if (Build.VERSION.SDK_INT >= 21)
-        {
-            int flag = window.getDecorView().getSystemUiVisibility();
-            flag = addFlag(flag, View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            flag = addFlag(flag, View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.getDecorView().setSystemUiVisibility(flag);
-
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        } else if (Build.VERSION.SDK_INT >= 19)
-        {
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-            //虚拟键盘也透明
-            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }
-    }
-
-    private static int addFlag(int original, int flag)
-    {
-        final int result = original | flag;
-        return result;
-    }
-
-    private static int clearFlag(int original, int flag)
-    {
-        final int result = original & (~flag);
-        return result;
-    }
-
-    /**
      * 状态栏高度
      *
      * @param context
@@ -85,8 +52,9 @@ public class FStatusBarUtils
      */
     public static int getStatusBarHeight(Context context)
     {
-        final int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        return resourceId > 0 ? context.getResources().getDimensionPixelSize(resourceId) : 0;
+        final Resources resources = context.getResources();
+        final int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        return resourceId > 0 ? resources.getDimensionPixelSize(resourceId) : 0;
     }
 
     /**
@@ -110,5 +78,17 @@ public class FStatusBarUtils
     public static boolean isStatusBarVisible(Window window)
     {
         return ((window.getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) == 0);
+    }
+
+    private static int addFlag(int original, int flag)
+    {
+        final int result = original | flag;
+        return result;
+    }
+
+    private static int clearFlag(int original, int flag)
+    {
+        final int result = original & (~flag);
+        return result;
     }
 }
