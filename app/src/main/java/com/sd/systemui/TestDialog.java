3a46374d95ee3b67b99541a550fce320de5ee898
zjun <565061763@qq.com>
2020-09-12 16:20:38 +0800

    demo

--- app/src/main/java/com/sd/systemui/TestDialog.java
+++ app/src/main/java/com/sd/systemui/TestDialog.java
@@ -1,12 +1,8 @@
 package com.sd.systemui;
 
 import android.app.Activity;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 
 import com.sd.lib.dialoger.impl.FDialoger;
-import com.sd.lib.systemui.navigationbar.FNavigationBarUtils;
 import com.sd.lib.systemui.statusbar.FStatusBarUtils;
 import com.sd.lib.utils.context.FResUtil;
 
@@ -17,32 +13,23 @@ public class TestDialog extends FDialoger
         super(activity, R.style.lib_dialoger_default);
         setPadding(0, 0, 0, 0);
         setContentView(R.layout.dialog_test);
+
+        FStatusBarUtils.setTransparent(getWindow(), false);
     }
 
     @Override
     protected void onStart()
     {
         super.onStart();
-        setTransparent();
+        getWindow().getAttributes().height = FResUtil.getScreenHeight();
     }
 
-    public void setTransparent()
+    @Override
+    public void show()
     {
-        FStatusBarUtils.setTransparent(getWindow());
-
-        final WindowManager.LayoutParams params = getWindow().getAttributes();
-        params.gravity = Gravity.TOP;
-//        params.height = ViewGroup.LayoutParams.MATCH_PARENT;
-        params.height = FResUtil.getScreenHeight() ;
-        getWindow().setAttributes(params);
-//        getWindow().getDecorView().setSystemUiVisibility(getOwnerActivity().getWindow().getDecorView().getSystemUiVisibility());
-    }
-
-//    @Override
-//    public void show()
-//    {
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
-//        super.show();
+        super.show();
+//        getWindow().getDecorView().setSystemUiVisibility(getOwnerActivity().getWindow().getDecorView().getSystemUiVisibility());
 //        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
-//    }
+    }
 }
