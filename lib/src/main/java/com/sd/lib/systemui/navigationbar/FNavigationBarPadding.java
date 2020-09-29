195390521ee7e4cc4c2881ffb4a473e072d07711
zjun <565061763@qq.com>
2020-09-29 15:40:26 +0800

    FNavigationBarPadding

--- lib/src/main/java/com/sd/lib/systemui/navigationbar/FNavigationBarPadding.java
+++ lib/src/main/java/com/sd/lib/systemui/navigationbar/FNavigationBarPadding.java
@@ -1,16 +1,32 @@
 package com.sd.lib.systemui.navigationbar;
 
 import android.view.View;
+import android.view.Window;
 
 import com.sd.lib.systemui.common.FSystemUIHandler;
 
 public class FNavigationBarPadding extends FSystemUIHandler
 {
+    private final Window mWindow;
+
+    public FNavigationBarPadding(Window window)
+    {
+        if (window == null)
+            throw new NullPointerException("window is null");
+        mWindow = window;
+    }
+
     @Override
     protected void checkView(View view)
     {
-        final int barHeight = FNavigationBarUtils.getNavigationBarHeight(view.getContext());
-        if (barHeight != view.getPaddingBottom())
-            view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), barHeight);
+        final Window window = mWindow;
+        final boolean isNavigationBarVisible = FNavigationBarUtils.isNavigationBarVisible(view.getContext());
+        final boolean isTransparent = FNavigationBarUtils.isTransparent(window);
+        if (isNavigationBarVisible && isTransparent)
+        {
+            final int barHeight = FNavigationBarUtils.getNavigationBarHeight(view.getContext());
+            if (barHeight != view.getPaddingBottom())
+                view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), barHeight);
+        }
     }
 }
